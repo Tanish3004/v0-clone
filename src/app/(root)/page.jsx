c6a433d3a4c069ced1df70e6e4ff961120783aad
Tanish Sharma <sharmatanish3004@gmail.com>
2026-05-23 15:03:02 +0530

    05 home page

--- src/app/(root)/page.jsx
+++ src/app/(root)/page.jsx
@@ -1,12 +1,33 @@
-import Image from "next/image";
-import { Button } from "@/components/ui/button";
-import { UserButton } from "@clerk/nextjs";
+import React from 'react'
+import Image from 'next/image'
+import ProjectForm from '@/modules/home/components/ProjectForm'
 
-export default function Home() {
+const page = () => {
   return (
-  <div className="flex flex-col items-center justify-center h-screen">
-    <Button>Test</Button>
-    <UserButton/>
-  </div>
-  );
+    <div className='flex items-center justify-center w-full px-4 py-8'>
+      <div className='max-w-5xl w-full'>
+        <section className='space-y-8 flex flex-col items-center'>
+           <div className='flex flex-col items-center'>
+             <Image 
+             src = { "/v0-logo-dark.png"}
+             width={100}
+             height={100}
+             alt="logo"
+             
+             />
+           </div>
+           <h1 className='text-2xl md:text-5xl font-bold text-center'>Build Something with ❤️</h1>
+           <p className='text-lg md:text-xl text-muted-foreground text-center'>
+            Create apps and websites by chatting with AI
+           </p>
+
+           <div className='max-w-3xl w-full'>
+             <ProjectForm/>
+           </div>
+        </section>
+      </div>
+    </div>
+  )
 }
+
+export default page
